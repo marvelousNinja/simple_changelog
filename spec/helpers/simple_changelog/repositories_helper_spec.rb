70afc1d164763a3622b59783886500ac5c99d2e5
Alexander Sologoub <alexsologoub@gmail.com>
2012-12-03 08:03:28 +0300

    Created simple tests for RepositoriesHelper.

--- spec/helpers/simple_changelog/repositories_helper_spec.rb
+++ spec/helpers/simple_changelog/repositories_helper_spec.rb
@@ -2,5 +2,10 @@ require 'spec_helper'
 
 module SimpleChangelog
   describe RepositoriesHelper do
+  	subject { helper }
+  	
+  	it { should respond_to(:long_date_format) }
+  	it { should respond_to(:highlight_commit_tags) }
+  	it { should respond_to(:current_version_tag) }
   end
 end
